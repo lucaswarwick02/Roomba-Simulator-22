19e890ad1529d0c49e504da29d6e802a916c1537
Lucas <lucaswarwick02@gmail.com>
2022-10-31 19:13:46 +0000

    Functions for Winning and Losing

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameState : MonoBehaviour
 {
     public int initialBattery = 10;
+    public int pointsNeeded = 3;
 
     [HideInInspector] public int battery = 0;
     [HideInInspector] public int points = 0;
@@ -20,15 +21,33 @@ public class GameState : MonoBehaviour
 
     public void DecreaseBattery (int amount) {
         battery -= amount;
+        checkGameState();
     }
     public void IncreaseBattery (int amount) {
         battery += amount;
+        checkGameState();
     }
 
     public void DecreasePoints (int amount) {
         points -= amount;
+        checkGameState();
     }
     public void IncreasePoints (int amount) {
         points += amount;
+        checkGameState();
+    }
+
+    private void checkGameState () {
+        if (battery <= 0) GameLose();
+
+        if (points >= pointsNeeded) GameWin();
+    }
+
+    public void GameWin () {
+        Debug.Log("You win!");
+    }
+
+    public void GameLose () {
+        Debug.Log("You lose!");
     }
 }
