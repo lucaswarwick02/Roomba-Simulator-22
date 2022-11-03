bf252040b84883af60c6aebf5574c76e9e3476c4
Lucas <lucaswarwick02@gmail.com>
2022-11-03 11:49:10 +0000

    Levels have TODO func for changing scene

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -32,23 +32,38 @@ public class MainMenuManager : MonoBehaviour
         week3Panel.SetActive(true);
     }
 
+    /// <summary>
+    /// Go back to the MainMenu panel.
+    /// </summary>
     public void Back () {
         DeactivateAllPanels();
         mainMenuPanel.SetActive(true);
     }
 
+    /// <summary>
+    /// Create a new save on persistent storage.
+    /// </summary>
     public void NewSave () {
         GameSave.NewSave();
         UpdateUI();
     }
+    /// <summary>
+    /// Saves data to persistent storage.
+    /// </summary>
     public void Save () {
         GameSave.Save();
     }
+    /// <summary>
+    /// Loads data from persistent storage.
+    /// </summary>
     public void Load() {
         GameSave.Load();
         UpdateUI();
     }
 
+    /// <summary>
+    /// (De)activates the UI based on the current GameSave data.
+    /// </summary>
     public void UpdateUI () {
         week1Option.GetComponent<Button>().enabled = GameSave.INSTANCE.week1Unlocked;
         week1Option.transform.GetChild(0).GetComponent<TextButton>().ToggleEffects(GameSave.INSTANCE.week1Unlocked);
@@ -73,6 +88,11 @@ public class MainMenuManager : MonoBehaviour
             week3Levels[i].GetComponent<Button>().enabled = GameSave.INSTANCE.week3LevelsUnlocked[i];
             week3Levels[i].transform.GetChild(0).GetComponent<TextButton>().ToggleEffects(GameSave.INSTANCE.week3LevelsUnlocked[i]);
         }
+    }
+
+    public void StartLevel (string levelID) {
+        // TODO Load level scene
+        Debug.Log("TODO: Load level " + levelID);
     }
 
     private void DeactivateAllPanels () {
