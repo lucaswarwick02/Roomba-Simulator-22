28f3daab5062daf87eb81a39c01e911990409d85
Lucas <lucaswarwick02@gmail.com>
2022-11-05 17:38:29 +0000

    Fix indentation + Issue #8

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public static PlayerMovement INSTANCE;
 
-    private Vector3Int nd = new Vector3Int(0,0,0);
+    private Vector3Int nd = new Vector3Int(0, 0, 0);
 
     private float movespeed;
     public Transform movePoint; //This is what gets moved instantly and the roomba sprite follows non instantly causing animation
@@ -17,86 +17,96 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3Int vel;
 
-    private void Awake() {
+    private void Awake()
+    {
         INSTANCE = this;
         movePoint.parent = null;
     }
 
-    private void Start() {
+    private void Start()
+    {
         movePoint.position = transform.position - TilemapManager.INSTANCE.offset;
         // transform.position = TilemapManager.INSTANCE.currentPos - TilemapManager.INSTANCE.offset;
         UpdatePlayerPosition(nd);
     }
 
-    public bool isMoving(){
-        if(Vector3.Distance(transform.position, movePoint.position) != 0f){
+    public bool isMoving()
+    {
+        if (Vector3.Distance(transform.position, movePoint.position) != 0f)
+        {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
 
-    public void updateSpeed(float speed){
+    public void updateSpeed(float speed)
+    {
         movespeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, 
+        transform.position = Vector3.MoveTowards(transform.position,
                 movePoint.position,
                 movespeed * Time.deltaTime);
 
-        if(!isMoving()){ // cant have tile effects act until roomba stops moving
-            if(wfe){
+        if (!isMoving())
+        { // cant have tile effects act until roomba stops moving
+            if (wfe)
+            {
                 wfe = false;
                 TilemapManager.INSTANCE.ProcessInput(movementDir);
             }
-            else{ 
-        updateSpeed(3f);
+            else
+            {
+                updateSpeed(3f);
 
-        if(GameState.INSTANCE.Battery > 0){
+                if (GameState.INSTANCE.Battery > 0)
+                {
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            wfe = true;
-            movementDir = new Vector3Int(0, 1, 0);
-            TilemapManager.INSTANCE.newPos(new Vector3Int(0, 1, 0));
-            GameState.INSTANCE.Battery -= 1;
-         
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            wfe = true;
-            movementDir = new Vector3Int(0, -1, 0);
-            TilemapManager.INSTANCE.newPos(new Vector3Int(0, -1, 0));
-            GameState.INSTANCE.Battery -= 1;
-          
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            wfe = true;
-            movementDir = new Vector3Int(-1, 0, 0);
-            TilemapManager.INSTANCE.newPos(new Vector3Int(-1,0, 0));
-            GameState.INSTANCE.Battery -= 1;
-        
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            wfe = true;
-            movementDir = new Vector3Int(1, 0, 0);
-            TilemapManager.INSTANCE.newPos(new Vector3Int(1, 0, 0));
-            GameState.INSTANCE.Battery -= 1;
-            
-        }
-        }
-        }
+                    if (Input.GetKeyDown(KeyCode.UpArrow))
+                    {
+                        wfe = true;
+                        movementDir = new Vector3Int(0, 1, 0);
+                        TilemapManager.INSTANCE.newPos(new Vector3Int(0, 1, 0));
+                        GameState.INSTANCE.Battery -= 1;
+
+                    }
+                    if (Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        wfe = true;
+                        movementDir = new Vector3Int(0, -1, 0);
+                        TilemapManager.INSTANCE.newPos(new Vector3Int(0, -1, 0));
+                        GameState.INSTANCE.Battery -= 1;
+
+                    }
+                    if (Input.GetKeyDown(KeyCode.LeftArrow))
+                    {
+                        wfe = true;
+                        movementDir = new Vector3Int(-1, 0, 0);
+                        TilemapManager.INSTANCE.newPos(new Vector3Int(-1, 0, 0));
+                        GameState.INSTANCE.Battery -= 1;
+
+                    }
+                    if (Input.GetKeyDown(KeyCode.RightArrow))
+                    {
+                        wfe = true;
+                        movementDir = new Vector3Int(1, 0, 0);
+                        TilemapManager.INSTANCE.newPos(new Vector3Int(1, 0, 0));
+                        GameState.INSTANCE.Battery -= 1;
+
+                    }
+                }
+            }
         }
 
     }
 
-    public void UpdatePlayerPosition (Vector3Int velocity)
+    public void UpdatePlayerPosition(Vector3Int velocity)
     {
         transform.position = TilemapManager.INSTANCE.currentPos - TilemapManager.INSTANCE.offset;
         // transform.position = Vector3.MoveTowards(TilemapManager.INSTANCE.currentPos, 
@@ -104,16 +114,18 @@ public class PlayerMovement : MonoBehaviour
         // movespeed * Time.deltaTime);
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.name.Equals("Effects")) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name.Equals("Effects"))
+        {
             // Roomba has collided with an collection tile
-            Vector3Int tilePos =  Vector3Int.FloorToInt(transform.position + TilemapManager.INSTANCE.offset);
+            Vector3Int tilePos = TilemapManager.INSTANCE.effectsTilemap.WorldToCell(transform.position);
             string tileName = TilemapManager.INSTANCE.effectsTilemap.GetTile(tilePos).name;
             TilemapManager.INSTANCE.PerformCollection(TilemapManager.TileNameToEnum(tileName), tilePos);
         }
     }
 }
-    
 
-    
+
+
 
