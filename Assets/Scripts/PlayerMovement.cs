1a803e34dd6c21633fde17f7fda28d14184e2e45
Lucas <lucaswarwick02@gmail.com>
2022-11-07 19:15:37 +0000

    Fix exception in build

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -82,6 +82,7 @@ public class PlayerMovement : MonoBehaviour
                 movePoint2.position,
                 movespeed2 * Time.deltaTime);
 
+
         if (!isMoving())
         { // cant have tile effects act until roomba stops moving
             if (wfe)
