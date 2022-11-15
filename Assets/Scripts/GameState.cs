ec7a84a6dc10334def9035c78b59e40225ba45e1
Elliot Parsons <ebp1g19@soton.ac.uk>
2022-11-15 16:20:09 +0000

    added music to menu

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System;
 
 public class GameState : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class GameState : MonoBehaviour
     public int initialBattery2 = 10;
     public int maxDirt = 3;
     public Level level;
+    public double passScore;
 
     public CompletionPanel completionPanel;
 
@@ -67,6 +69,8 @@ public class GameState : MonoBehaviour
     }  
 
     private void Start() {
+        passScore = Math.Ceiling((double) (maxDirt/2));
+        Dirt = maxDirt;
         // Change scale of camera to match settings
         Camera.main.orthographicSize  = settings.largeScale ? 5f : 10f;
     }
@@ -103,7 +107,7 @@ public class GameState : MonoBehaviour
             // Both Roombas have died! (Lose)
             isGameOver = true;
         }
-        if (Dirt >= maxDirt) {
+        if (Dirt <= 0) {
             // They have collected all the dirt! (Win)
             isGameOver = true;
         }
@@ -116,10 +120,12 @@ public class GameState : MonoBehaviour
             return;
         }
         else {
+            
             gameOver = true;
         }
 
-        float score = ((float) Dirt - (float) Rings) / (float) maxDirt;
+        float score = (((float) maxDirt - (float) Dirt) - (float) Rings);
+        Debug.Log(score);
 
         // Assign score to level save data
         switch (level.week) {
@@ -136,18 +142,16 @@ public class GameState : MonoBehaviour
                 break;
         }
 
-        // Completion Panel
-        completionPanel.gameObject.SetActive(true);
-        completionPanel.resultsTitle.text = "Level " + level.week + "-" + level.day + " Results:";
-        completionPanel.dirtCollected.text = Dirt.ToString();
-        completionPanel.ringsCollected.text = Rings.ToString();
-        completionPanel.dirtScore.text = "+" + Dirt;
-        completionPanel.ringsScore.text = "-" + Rings;
-        completionPanel.score.text = "Score: " + (Dirt - Rings) + "/" + maxDirt;
-        completionPanel.level = level;
-
-        if (score >= 0.5f)
+        if (score > passScore)
         {
+
+
+            // * Completion Panel = Win
+            completionPanel.gameObject.SetActive(true);
+            completionPanel.titleText.text = "You Win!";
+            completionPanel.percentageText.text = (score).ToString("#");
+            completionPanel.percentageText.color = Color.green;
+
             // Unlock next level
             Level nextLevel = level.NextLevel();
             switch (nextLevel.week)
@@ -165,15 +169,18 @@ public class GameState : MonoBehaviour
                     break;
             }
         }
-        else {
-            completionPanel.deactivateNextLevel();
-            completionPanel.redFlag.color = new Color(1f, 1f, 1f, 0.5f);
+        else
+        {
+            // * Completion Panel = Lose
+            completionPanel.gameObject.SetActive(true);
+            completionPanel.titleText.text = "You Lose";
+            if(score <= 0){
+                completionPanel.percentageText.text = (0).ToString("#");
+            }
+            else{
+                completionPanel.percentageText.text = (score).ToString("#");
+            }
+            completionPanel.percentageText.color = Color.red;
         }
-
-        if (score < 1f) {
-            completionPanel.goldFlag.color = new Color(1f, 1f, 1f, 0.5f);
-        }
-
-        if (level.week == 3 && level.day == 5) completionPanel.deactivateNextLevel();
     }
 }
--- Assets/Scripts/UI/CompletionPanel.cs
+++ Assets/Scripts/UI/CompletionPanel.cs
@@ -7,39 +7,10 @@ using UnityEngine.SceneManagement;
 
 public class CompletionPanel : MonoBehaviour
 {
-    public TextMeshProUGUI resultsTitle;
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI percentageText;
 
-    public TextMeshProUGUI dirtCollected;
-    public TextMeshProUGUI ringsCollected;
-    public TextMeshProUGUI dirtScore;
-    public TextMeshProUGUI ringsScore;
-
-    public TextMeshProUGUI score;
-
-    [HideInInspector] public Level level;
-
-    public Button nextLevelButton;
-    public TextButton nextLevelText;
-
-    public Image redFlag;
-    public Image goldFlag;
-
-    public void deactivateNextLevel () {
-        nextLevelButton.enabled = false;
-        nextLevelText.ToggleEffects(false);
-        nextLevelText.ForceDisable();
-    }
-
-    public void NextLevel () {
-        Level nextLevel = level.NextLevel();
-        SceneManager.LoadScene("Level_" + nextLevel.week + "-" + nextLevel.day);
-    }
-
-    public void RetryLevel () {
-        SceneManager.LoadScene("Level_" + level.week + "-" + level.day);
-    }
-
-    public void MainMenu () {
+    public void BackToMainMenu () {
         SceneManager.LoadScene("MainMenu");
     }
 }
