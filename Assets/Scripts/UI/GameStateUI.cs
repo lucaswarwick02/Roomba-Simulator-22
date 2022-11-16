02f8e73ebdc4bcfa99dfab0b5b9125b6ccd1582e
Elliot Parsons <ebp1g19@soton.ac.uk>
2022-11-16 19:28:54 +0000

    added battery coloring

--- Assets/Scripts/UI/GameStateUI.cs
+++ Assets/Scripts/UI/GameStateUI.cs
@@ -12,6 +12,12 @@ public class GameStateUI : MonoBehaviour
     public TextMeshProUGUI roomba2Text;
     public TextMeshProUGUI batteryCount2;
 
+    private Color dead = new Color(0.5377358f, 0.5377358f, 0.5377358f);
+    private Color red = new Color(0.8f, 0f, 0.1f);
+    private Color orange = new Color(0.8f, 0.6f, 0.2f);
+    private Color green = new Color(0.4f, 0.8f, 0.2f);
+
+
     public TextMeshProUGUI dirtCount;
     public TextMeshProUGUI ringsCount;
 
@@ -22,17 +28,39 @@ public class GameStateUI : MonoBehaviour
     {
         if(GameState.INSTANCE.Battery1 <= 0){
             batteryCount.text = "Battery Life: " + 0;
+            batteryCount.color = dead;
         }
         else{
             batteryCount.text = "Battery Life: " + GameState.INSTANCE.Battery1;
         }
+        if(GameState.INSTANCE.Battery1 == 1){
+            batteryCount.color = red;
+        }
+        if(GameState.INSTANCE.Battery1 < 3 && GameState.INSTANCE.Battery1 > 1){
+            batteryCount.color = orange;
+        }
+        if(GameState.INSTANCE.Battery1 > 3){
+            batteryCount.color = green;
+
+        }
         if(GameState.INSTANCE.Battery2 <= 0){
             batteryCount2.text = "Battery Life: " + 0;
+            batteryCount2.color = dead;
         }
         else{
             batteryCount2.text = "Battery Life: " + GameState.INSTANCE.Battery2;
         }
+        if(GameState.INSTANCE.Battery2 == 1){
+            batteryCount2.color = red;
+        }
+        if(GameState.INSTANCE.Battery2 < 3 && GameState.INSTANCE.Battery2 > 1){
+            batteryCount.color = orange;
+        }
+        if(GameState.INSTANCE.Battery2 > 3){
+            batteryCount2.color = green;
 
+        }
+       
         dirtCount.text = "Dirt Left: " + GameState.INSTANCE.Dirt;
         ringsCount.text = "Rings Vacuumed: " + GameState.INSTANCE.Rings;
 
